fa956a4a9b7483ea096a39893f73e6eb5aed71a8
Shawn Miranda <theshawnmiranda@gmail.com>
2023-08-05 16:38:48 -0500

    more notes

--- src/main.rs
+++ src/main.rs
@@ -46,39 +46,39 @@ fn main() {
 
 }
 
-/*
+/*/
     Variables are immutable by default. Using the mut keyword makes them mutable. You can change the value but not the type.
-*/
 
-/*
+
+
     Constants cannot be the result of a function but may be the result of a math caluclation.
     Constants are always immutable.
     Constants can be declared in any scope, including the global scope.
-*/
 
-/* 
+
+ 
     Shadowing is different from marking a variable as mut, because we’ll get a compile-time error
     if we accidentally try to reassign to this variable without using the let keyword or by trying to change it's type.
-*/
 
-/* 
+
+ 
     An integer can be signed (i.e. positive or negative) or unsigned (i.e. positive only).
     An i8 can store numbers from -128 to 127 inclusive, whereas a u8 can store numbers from 0 to 255 inclusive.
-*/
 
-/*
+
+
     In signed numbers, the first bit is used to indicate whether the number is positive or negative. 1 is negative, 0 is positive.
-*/
 
-/*
+
+
     Java's double is Rust's f64. Rust also has f32 which is Java's float.
-*/
 
-/*
+
+
     Boolean type is bool.
-*/
 
-/*
+
+
     Compound types can group multiple values into one type.
     Rust has two primitive compound types: tuples and arrays (fixed length).
     Tuples === (,) === Different types. Empty tuples are called unit.
