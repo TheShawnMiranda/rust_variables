a22a2212241cbbfc8b2ec47d248406bdc304c39f
Shawn Miranda <theshawnmiranda@gmail.com>
2023-08-05 14:32:24 -0500

    more docs

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,8 @@ fn main() {
     // const MAX_POINTS: u32 = 100_000;
     // println!("The value of MAX_POINTS is: {MAX_POINTS}");
 
-    let x = 5;
-
-    let x = x + 1;
-
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {x}");
-    }
-
-    println!("The value of x in the outer scope is: {x}");
-
-
-    // {
-    //     let x = x * 2;
-    //     println!("The value of x in the inner scope is: {x}");
-    // }
+    let x: u8 = 255;
+    println!("The value of x is: {x}");
 
 
 }
@@ -39,4 +25,17 @@ fn main() {
 /* 
     Shadowing is different from marking a variable as mut, because we’ll get a compile-time error
     if we accidentally try to reassign to this variable without using the let keyword or by trying to change it's type.
+*/
+
+/* 
+    An integer can be signed (i.e. positive or negative) or unsigned (i.e. positive only).
+    An i8 can store numbers from -128 to 127 inclusive, whereas a u8 can store numbers from 0 to 255 inclusive.
+*/
+
+/*
+    In signed numbers, the first bit is used to indicate whether the number is positive or negative. 1 is negative, 0 is positive.
+*/
+
+/*
+    Java's double is Rust's f64. Rust also has f32 which is Java's float.
 */
